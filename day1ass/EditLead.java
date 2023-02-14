698fc8797175c25a139b4584f2651572e0f88614
Sandhiya-3910 <124510202+Sandhiya-3910@users.noreply.github.com>
2023-02-14 13:10:12 +0530

    Add files via upload
    Week2.day 1 assignment uploaded

---
+++ day1ass/EditLead.java
@@ -0,0 +1,43 @@
+package week2.day1ass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditLead {
+	
+	public static void main(String[] args) {
+		ChromeDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Cognizant");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Sandhiya");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Satheeshkumar");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Sandy");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Employee");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("sandysatheesh2699@gmail.com");
+		WebElement State = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		
+		Select st=new Select(State);
+		st.selectByVisibleText("New York");
+		driver.findElement(By.className("smallSubmit")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Description field has been removed");
+		driver.findElement(By.className("smallSubmit")).click();
+        String title=driver.getTitle(); 
+        System.out.println(title);
+        
+	}
+        
+		
+
+}
